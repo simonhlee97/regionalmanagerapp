748cf21ea2ae5fa3a25935ebce65e293735e649e
Simon Lee <simonhlee97@hotmail.com>
2020-04-15 11:12:51 +0900

    Deploy

---
+++ precache-manifest.270bab441827d74e061967ce58920f14.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ecbe12577e83624c65",
+    "url": "/regionalmanagerapp/css/app.a9e28a8e.css"
+  },
+  {
+    "revision": "35de31e771ddebcdbcbd",
+    "url": "/regionalmanagerapp/css/chunk-vendors.f3b3c341.css"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/regionalmanagerapp/fonts/open-sans-v15-latin-300.177cc92d.ttf"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/regionalmanagerapp/fonts/open-sans-v15-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/regionalmanagerapp/fonts/open-sans-v15-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "76b56857ebbae3a5a689f213feb11af0",
+    "url": "/regionalmanagerapp/fonts/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/regionalmanagerapp/fonts/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/regionalmanagerapp/fonts/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/regionalmanagerapp/fonts/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/regionalmanagerapp/fonts/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/regionalmanagerapp/fonts/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/regionalmanagerapp/fonts/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/regionalmanagerapp/fonts/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/regionalmanagerapp/fonts/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "2d2afb2719a1ee903e576e7c457daf81",
+    "url": "/regionalmanagerapp/fonts/primeicons.2d2afb27.eot"
+  },
+  {
+    "revision": "66ee0deb739ca71f0ecdc39d7c1b22cb",
+    "url": "/regionalmanagerapp/fonts/primeicons.66ee0deb.woff"
+  },
+  {
+    "revision": "df0140f8e79ecfeffaf85220aaecd7c4",
+    "url": "/regionalmanagerapp/fonts/primeicons.df0140f8.ttf"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/regionalmanagerapp/img/color.c7a33805.png"
+  },
+  {
+    "revision": "0c5717cf938c7ca3b94c8b302d85304a",
+    "url": "/regionalmanagerapp/img/dunder.0c5717cf.jpg"
+  },
+  {
+    "revision": "0c5717cf938c7ca3b94c8b302d85304a",
+    "url": "/regionalmanagerapp/img/dunder.jpg"
+  },
+  {
+    "revision": "e255b0e3f3c70bc0953d14ad78a02dc9",
+    "url": "/regionalmanagerapp/img/mike1.jpg"
+  },
+  {
+    "revision": "5dbc3e3c34cc1528d49c4052d01a0f95",
+    "url": "/regionalmanagerapp/img/mike2.5dbc3e3c.jpg"
+  },
+  {
+    "revision": "5dbc3e3c34cc1528d49c4052d01a0f95",
+    "url": "/regionalmanagerapp/img/mike2.jpg"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/regionalmanagerapp/img/open-sans-v15-latin-300.27ef0b06.svg"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/regionalmanagerapp/img/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/regionalmanagerapp/img/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "e5e0e94474d5fd92e7e800a8865d297c",
+    "url": "/regionalmanagerapp/img/primeicons.e5e0e944.svg"
+  },
+  {
+    "revision": "170f8f4151f2b93ec22d33a2673c746d",
+    "url": "/regionalmanagerapp/img/scranton.170f8f41.jpg"
+  },
+  {
+    "revision": "170f8f4151f2b93ec22d33a2673c746d",
+    "url": "/regionalmanagerapp/img/scranton.jpg"
+  },
+  {
+    "revision": "28bf0ec9bf0a4618310239845e5858b3",
+    "url": "/regionalmanagerapp/index.html"
+  },
+  {
+    "revision": "05ecbe12577e83624c65",
+    "url": "/regionalmanagerapp/js/app.231ba2c1.js"
+  },
+  {
+    "revision": "35de31e771ddebcdbcbd",
+    "url": "/regionalmanagerapp/js/chunk-vendors.7e6c4a95.js"
+  },
+  {
+    "revision": "5a14253fbce95dedc833e0df713bc38b",
+    "url": "/regionalmanagerapp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/regionalmanagerapp/robots.txt"
+  }
+]);
